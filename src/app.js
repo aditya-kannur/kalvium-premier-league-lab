98310dfbc434c1b693ac160ef169d96f72eee378
AdityaKannur <adityakannur28@gmail.com>
2024-03-11 16:44:21 +0530

    ok

--- src/app.js
+++ src/app.js
@@ -6,11 +6,18 @@ let trophiesWon = 27;
 
 //Write your function here
 
+function createManager(managerName, managerAge, currentTeam, trophiesWon)
+ {
+  var Manager = [managerName, managerAge, currentTeam, trophiesWon]
+  return Manager;
+
+}
+
 // Don't edit the following code
 try {
   var manager = createManager(
     managerName,
-    managerAge,
+    managerAge, 
     currentTeam,
     trophiesWon
   );
@@ -23,6 +30,22 @@ var formation = [4, 4, 3];
 
 //write your function here
 
+function createFormation(formation){
+
+  if(formation.length==0)
+  {
+  return null;
+  }
+  var play =
+  {
+    defender:formation[0],
+    midfield: formation[1],
+    forward: formation[2]
+}
+  return play;
+
+}
+
 // Dont edit the following code
 
 try {
@@ -33,21 +56,99 @@ try {
 
 //Progression 3 - Filter players that debuted in ___ year
 
+function filterByDebut(year){
+  var data = players.filter(player => player.debut == year); 
+  return data
+}
+
 //Progression 4 - Filter players that play at the position _______
+function filterByPosition(position){
+  var data = players.filter(player => player.position == position)
+  return data
+}
 
 //Progression 5 - Filter players that have won ______ award
+function filterByAward(awardName) {
+  let data = [];
+  for (let i = 0; i < players.length; i++) {
+    for (let j = 0; j < players[i].awards.length; j++) {
+      if (players[i].awards[j].name === awardName) {
+        data.push(players[i]);
+      }
+    }
+  }
+  return data;
+}
 
 //Progression 6 - Filter players that won ______ award ____ times
+function filterByAwardxTimes(awardName, noOfTimes) {
+  let data = [];
+  let count = 0;
+  for (let i = 0; i < players.length; i++){
+      for (let j = 0; j < players[i].awards.length; j++) {
+        if (players[i].awards[j].name == awardName){
+            count ++;       
+        }
+      }
+    if (count == noOfTimes){
+      data.push(players[i]);
+    }
+    count = 0;
+  }
+  return data;
+}
 
 //Progression 7 - Filter players that won ______ award and belong to ______ country
+function filterByAwardxCountry(awardName, country){
+  let data = [];
+  for (let i = 0; i < players.length; i++) {
+    for (let j = 0; j < players[i].awards.length; j++) {
+      if (players[i].awards[j].name === awardName && players[i].country == country) {
+        data.push(players[i]);
+      }
+    }
+  }
+  return data;
+}
 
 //Progression 8 - Filter players that won atleast ______ awards, belong to ______ team and are younger than ____
+function filterByNoOfAwardsxTeamxAge(noOfAwards, team, age) {
+  var data = players.filter(player => (player.age < age && player.team == team && player.awards.length >= noOfAwards));
+  return data;
+}
 
 //Progression 9 - Sort players in descending order of their age
+function sortByAge(players) {
+  players.sort((a, b) => b.age - a.age);
+  return players;
+}
+
 
 //Progression 10 - Sort players beloging to _____ team in descending order of awards won
+function filterByTeamAndSortByNoOfAwards(players, team) {
+  const teamPlayers = players.filter(player => player.team === team);
+  teamPlayers.sort((a, b) => b.awards - a.awards);
+  return teamPlayers;
+}
 
 //Challenge 1 - Sort players that have won _______ award _____ times and belong to _______ country in alphabetical order of their names
+function compare(a, b) {
+  let value = 0;
+  if (a.name > b.name) {
+    value = 1;
+  }
+  else if (a.name < b.name) { value = -1; }
+  else { value = 0; }
+}
+function SortByNamexAwardxTimes(awardName, noOfTimes, country) {
+  var dataOne = filterByAwardxTimes(awardName, noOfTimes);
+  var dataTwo = dataOne.filter(player => player.country == country);
+  return dataTwo.sort(compare);
+}
 
 //Challenge 2 - Sort players that are older than _____ years in alphabetical order
 //Sort the awards won by them in reverse chronological order
+function SortByNamexOlderThan(age) {
+  var data = players.filter(player => player.age > age);
+  return data;
+}
